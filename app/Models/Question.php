cd9267362a4be1c7a6a3c11ab2896694875e0527
Reza <reza67asgari@gmail.com>
2020-09-21 15:16:07 +0330

    show questions page

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -23,4 +23,9 @@ class Question extends Model
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = Str::slug($value);
     }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
 }
---
+++ resources/views/question/index.blade.php
@@ -0,0 +1,23 @@
+@extends('question.master')
+    @section('content')
+        <div class="container">
+            @foreach ($questions as $question)
+                <div class="my-3 p-3 bg-white rounded shadow-sm">
+                    <h6 class="border-bottom border-gray pb-2 mb-0"><a href="{{route('questions.show', $question->id)}}">{{$question->title}}</a></h6>
+                    <div class="media text-muted pt-3">
+                    <svg class="bd-placeholder-img mr-2 rounded" width="32" height="32" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 32x32"><title>Placeholder</title><rect width="100%" height="100%" fill="#007bff"/><text x="50%" y="50%" fill="#007bff" dy=".3em">32x32</text></svg>
+                    <p class="media-body pb-3 mb-0 small lh-125 border-bottom border-gray">
+                        <small>Asked by:</small><strong class="d-block text-gray-dark">{{$question->user->name}}</strong><small>{{$question->created_date}}</small>
+                    </p>
+                    <p class="media-body">
+                        {{Str::limit($question->body, 250)}}
+                    </p>
+                    </div>
+                </div>
+            @endforeach
+
+            {{$questions->links()}}
+        </div>
+
+    @endsection
+
